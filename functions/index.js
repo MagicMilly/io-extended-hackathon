a605b17046478bc44a096eeefc02219b81dc6f7a
MagicMilly <emilycain@ymail.com>
2019-07-07 08:12:26 -0700

    added one more level of convo to choose excuse about instruments

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,14 @@
 'use strict';
 
-// Import the Dialogflow module from the Actions on Google client library.
-const {dialogflow} = require('actions-on-google');
+// // Import the Dialogflow module from the Actions on Google client library.
+// const {dialogflow} = require('actions-on-google');
 
-// // Import the Dialogflow module and response creation dependencies from the 
-// // Actions on Google client library.
-// // const {
-// //     dialogflow,
-// //     Permission,
-// //     Suggestions,
-// //     BasicCard,
-// //   } = require('actions-on-google');
+// Import the Dialogflow module and response creation dependencies from the 
+// Actions on Google client library.
+const {
+    dialogflow,
+    Suggestions,
+  } = require('actions-on-google');
 
 // Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
@@ -20,15 +18,28 @@ const app = dialogflow({debug: true});
 
 const welcome_intent = 'Default Welcome Intent';
 const fallback_intent = 'Default Fallback Intent';
-const need_excuse_intent = 'NeedExcuse';
+// I know this looks terrible, will fix later.
+const instrument_excuse = 'Default Welcome Intent - yes - yes';
 
 app.intent(welcome_intent, (conv) => {
-    conv.ask("Hi There! Do you have an event coming up which you would like to avoid?");
+    conv.ask('Hi There! Do you have an event coming up which you would like to avoid?');
+    conv.ask(new Suggestions('Yes', 'No'));
 });
 
+// User does not need an excuse. 
 app.intent('Default Welcome Intent - no', (conv) => {
-    // I am just trying to hard code something that will actually work, please work 
+    // End conversation. 
     conv.close('Okay. Goodbye!');
+});
+
+// User needs an excuse.
+app.intent('Default Welcome Intent - yes', (conv) => {
+    conv.ask('Okay. Would you like an excuse about musical instruments?');
+});
+
+// User has chosen an excuse about musical instruments.
+app.intent(instrument_excuse, (conv) => {
+    conv.close('Alright. You could say that you need to tune your guitar.');
 });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
